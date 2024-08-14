38936c5501e9f7753bfafd51d0818a91edb28e80
Borov242 <103368670+Borov242@users.noreply.github.com>
2024-08-14 17:31:07 +0300

    Add files via upload

---
+++ Платформер/sprites/Coin_Gems/MonedaD.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="MonedaD" tilewidth="80" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="MonedaD.png" width="80" height="16"/>
+ </tile>
+</tileset>
